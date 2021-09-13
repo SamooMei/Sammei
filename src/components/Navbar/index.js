4cbd256b0c65dd607d30e00135cce086a61da6df
SamooMei <Samoooomei@gmail.com>
2021-09-13 15:07:40 -0400

    Basic Routing

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,34 +1,42 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  RiGithubLine,
-  RiLinkedinLine,
-  RiArrowUpSLine,
-  RiArrowDownSLine,
-} from "react-icons/ri";
-import "./index.scss";
+	RiBriefcase3Line,
+	RiHomeHeartLine,
+	RiGithubLine,
+	RiLinkedinLine,
+	RiArrowUpSLine,
+	RiArrowDownSLine,
+} from 'react-icons/ri';
+import './index.scss';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+	const [nav, setNav] = useState(false);
 
-  return (
-    <div className={"navbar"}>
-      <div className={"navbar__switch"}>
-        {nav ? (
-          <RiArrowUpSLine className={"icons"} onClick={() => setNav(!nav)} />
-        ) : (
-          <RiArrowDownSLine className={"icons"} onClick={() => setNav(!nav)} />
-        )}
-      </div>
-      <div className={`navbar__content ${nav ? "active" : ""}`}>
-        <a href={"https://github.com/SamooMei"}>
-          <RiGithubLine className={"icons"} />
-        </a>
-        <a href={"https://linkedin.com/in/samuelmei/"}>
-          <RiLinkedinLine className={"icons"} />
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div className={'navbar'}>
+			<div className={'navbar__switch'}>
+				{nav ? (
+					<RiArrowUpSLine className={'icons'} onClick={() => setNav(!nav)} />
+				) : (
+					<RiArrowDownSLine className={'icons'} onClick={() => setNav(!nav)} />
+				)}
+			</div>
+			<div className={`navbar__content ${nav ? 'active' : ''}`}>
+				<a href={'/'}>
+					<RiHomeHeartLine className={'icons'} />
+				</a>
+				<a href={'/Projects'}>
+					<RiBriefcase3Line className={'icons'} />
+				</a>
+				<a href={'https://github.com/SamooMei'}>
+					<RiGithubLine className={'icons'} />
+				</a>
+				<a href={'https://linkedin.com/in/samuelmei/'}>
+					<RiLinkedinLine className={'icons'} />
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
-import "./index.scss";
+import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path="/" exact component={Home} />
+			</Switch>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
