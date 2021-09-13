b6ec3d0ff917134df046c5225735d9d3e101fca5
SamooMei <Samoooomei@gmail.com>
2021-09-13 02:01:21 -0400

    Added Introduction

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Home from './Home';
 
 const App = () => {
 	return (
 		<div className="App">
 			<Navbar />
+			<Home/>
 		</div>
 	);
 };
