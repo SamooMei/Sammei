f53f3cfb3562f82e113e6e7ddde1bc57f02a0d4d
SamooMei <Samoooomei@gmail.com>
2021-09-15 19:15:58 -0400

    More Navbar Stuff

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,57 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-	RiBriefcase3Line,
-	RiHomeHeartLine,
-	RiGithubLine,
-	RiLinkedinLine,
-	RiArrowUpSLine,
-	RiArrowDownSLine,
-} from 'react-icons/ri';
-import './index.scss';
+  RiBriefcase3Line,
+  RiHomeHeartLine,
+  RiGithubLine,
+  RiLinkedinLine,
+  RiArrowUpSLine,
+  RiArrowDownSLine,
+} from "react-icons/ri";
+import "./index.scss";
 
 const Navbar = () => {
-	const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
 
-	return (
-		<div className={'navbar'}>
-			<div className={'navbar__switch'}>
-				{nav ? (
-					<RiArrowUpSLine className={'icons'} onClick={() => setNav(!nav)} />
-				) : (
-					<RiArrowDownSLine className={'icons'} onClick={() => setNav(!nav)} />
-				)}
-			</div>
-			<div className={`navbar__content ${nav ? 'active' : ''}`}>
-				<div className={'navbar__content__ref'}>
-					<a href={'/'} className={'home'}>
-						<RiHomeHeartLine className={'icons'} />
-					</a>
-					<div className={'names home_'}>Home</div>
-				</div>
+  return (
+    <div className={"navbar"}>
+      <div className={"navbar__switch"}>
+        {nav ? (
+          <RiArrowUpSLine className={"icons"} onClick={() => setNav(!nav)} />
+        ) : (
+          <RiArrowDownSLine className={"icons"} onClick={() => setNav(!nav)} />
+        )}
+      </div>
+      <div className={`navbar__content ${nav ? "active" : ""}`}>
+        <div className={"navbar__content__ref"}>
+          <a href={"/"} className={"home"}>
+            <RiHomeHeartLine className={"icons"} />
+          </a>
+          <div className={"names home_"}>Home</div>
+        </div>
 
-				<div className={'navbar__content__ref'}>
-					<a href={'/Projects'} className={'projects'}>
-						<RiBriefcase3Line className={'icons'} />
-					</a>
-					<div className={'names projects_'}>Projects</div>
-				</div>
+        <div className={"navbar__content__ref"}>
+          <a href={"/Projects"} className={"projects"}>
+            <RiBriefcase3Line className={"icons"} />
+          </a>
+          <div className={"names projects_"}>Projects</div>
+        </div>
 
-				<div className={'navbar__content__ref'}>
-					<a href={'https://github.com/SamooMei'} className={'github'}>
-						<RiGithubLine className={'icons'} />
-					</a>
-					<div className={'names github_'}>Github</div>
-				</div>
+        <div className={"navbar__content__ref"}>
+          <a href={"https://github.com/SamooMei"} className={"github"}>
+            <RiGithubLine className={"icons"} />
+          </a>
+          <div className={"names github_"}>Github</div>
+        </div>
 
-				<div className={'navbar__content__ref'}>
-					<a href={'https://linkedin.com/in/samuelmei/'} className={'linkedin'}>
-						<RiLinkedinLine className={'icons'} />
-					</a>
-					<div className={'names  linkedin_'}>LinkedIn</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className={"navbar__content__ref"}>
+          <a href={"https://linkedin.com/in/samuelmei/"} className={"linkedin"}>
+            <RiLinkedinLine className={"icons"} />
+          </a>
+          <div className={"names  linkedin_"}>LinkedIn</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
