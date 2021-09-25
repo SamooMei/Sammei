432f25d36dc2f7f4847e83eba1b6b1763b7a6bc1
SamooMei <Samoooomei@gmail.com>
2021-09-25 13:06:40 -0400

    Added YACS

--- src/data/index.js
+++ src/data/index.js
@@ -6,13 +6,20 @@ const works = [
 		github: 'https://github.com/OpenCircuits/OpenCircuits'
 	},
 	{
+		name: 'YACS.N',
+		description:
+			'RPI Course Planner with support for multi semester and partial semesters',
+		link: 'https://yacs.cs.rpi.edu/',
+		github: 'https://github.com/YACS-RCOS/yacs.n',
+	},
+	{
 		name: 'HASSPathways',
 		description:
 			'Web application led by Bram Van Heuveln that allows students to navigate through their selected HASS pathway courses ',
 		link: 'https://hasspathways.netlify.app/',
 		github: '',
 	},
-		{
+	{
 		name: 'Pomall',
 		description: 'Pomodoro Timer based in ReactJS',
 		link: 'https://Pomall.xyz',
